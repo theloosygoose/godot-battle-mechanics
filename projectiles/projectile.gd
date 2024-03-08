72396874c617229bae9a637ee24ba455cd67061e
Gusti Rama Henry <g.ramahenry2439@gmail.com>
2024-03-08 14:42:08 -0500

    load default projectile texture

--- projectiles/projectile.gd
+++ projectiles/projectile.gd
@@ -9,8 +9,12 @@ var speed: float
 var direction: Vector2 = Vector2.ZERO 
 var initial_position: Vector2
 
+var imported_texture: Texture2D
 
 func _ready() -> void:
+	assign_texture()
+
+	
 	position = initial_position
 
 func _physics_process(delta: float) -> void:
@@ -25,4 +29,16 @@ func take_damage(damage_taken: int) -> void:
 func check_health() -> void:
 	if proj_health <= 0:
 		queue_free()
-		
+
+func assign_texture() -> void:		
+	if !imported_texture:
+
+		imported_texture = load("res://projectiles/assets/white_pill.png")
+		push_error("Texture Not Found Loading Default Texture")
+
+	var sprite2D: Sprite2D
+
+	for child in get_children():
+		if child is Sprite2D:
+			sprite2D = child
+			sprite2D.texture = imported_texture
